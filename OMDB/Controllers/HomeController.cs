e9569d38177ee29c9e7d5d300adfc3e12b76407e
AliAlHashemi14 <alialhashemi14@gmail.com>
2022-07-26 23:10:38 -0400

    first commit

---
+++ OMDB/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OMDB.Models;
+using System.Diagnostics;
+
+namespace OMDB.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public MovieDAL movieDAL = new MovieDAL();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet]//Run when you first get to page
+        public IActionResult MovieSearch()
+        {
+            return View();
+        }
+
+        [HttpPost]//Run when you post the stuffs
+        public IActionResult MovieSearch(string title)
+        {
+            MovieModel movie = movieDAL.GetMovie(title);
+            return View(movie);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
